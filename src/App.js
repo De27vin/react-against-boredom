794a2bc5eb673426d8987ee5bc76099a81766d1f
Devin de Spindler <devin.vipxxx@gmail.com>
2024-08-28 14:39:54 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+
+export default function Home() {
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect( () => {
+    console.log("Counter has been incremented by 1!");
+    console.log("New counter is ", counter);
+
+
+  })
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1>
+      <p>The counter is: {counter}</p>
+      <button className='incrementButton' onClick={() => setCounter(counter + 1)}>+1</button>
+
     </div>
   );
 }
-
-export default App;
